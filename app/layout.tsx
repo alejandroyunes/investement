09a0d7826e2fb120a2a6f7b14f4a1f3357270ac7
Alejandro Yunes <alejandroyunes@outlook.com>
2024-01-23 14:43:36 -0500

    fixing meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css"
 import { ThemeProvider } from "./providers"
 
 export const metadata = {
-  title: "Capital Invest",
-  description: "Capital invest company",
+  title: "Estrella Immobilien GmbH",
+  description: "Estrella Immobilien GmbH",
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
